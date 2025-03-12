b062327242648925ffb12203cca1ab9a87818814
Kumar Raj Bogireddy <kumarrajbogireddy29@gmail.com>
2025-03-12 23:02:10 +0530

    Add files via upload

---
+++ Untitled3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+char set_grade(int marks){
+	if (marks>= 90) return 'A';
+	else if (marks>=80 && < 90 ) return 'B';
+	else if (marks>70  && < 80) return 'C';
+	else if ( marks>60 && < 70) return 'D';
+	else if printf("E\n");
+}
+int main()
+  char stu1 = 75, stu2 =90 , stu3 =45;
+  set_grade(stu1);
+  set_grade(stu2);
+  set_grade(stu3);
+}
