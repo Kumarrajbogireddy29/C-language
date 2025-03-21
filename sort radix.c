187373cad0ce2d099018d2ef79ab0f034fba2e2f
Kumar Raj Bogireddy <kumarrajbogireddy29@gmail.com>
2025-03-21 23:11:59 +0530

    Add files via upload

---
+++ sort radix.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+void printarray(int *arr,int n)
+{  int i;
+	for(i=0;i<n;i++){
+	printf("%d ",arr[i]);
+}
+}
+int get_max(int *arr,int n)
+{int i;
+	int max=0;
+	for(i=0;i<n;i++)
+	{
+		if (arr[i]>max){
+			max=arr[i];
+		}
+	}
+		return max;			
+}
+void radix_sort(int*arr,int n){
+	int max =get_max(arr,n);
+	int e=1;
+	while(max)
+	{
+		int counts[10]={0};
+		int buckets[10][n];
+		int i,j;
+		for(i=0;i<n;i++){
+			int place=(arr[i]/e)%10;
+			buckets[place][counts[place]++]=arr[i];
+		}
+		int k=0;
+		for(i=0;i<10;i++){
+			for(j=0;j<counts[i];j++){
+				arr[k++]=buckets[i][j];
+				
+				
+			}
+		
+		}
+			e=e*10;
+			max /=10;
+		
+	}
+printarray(arr,n);
+}
+int main(){
+
+int n;
+scanf("%d",&n);
+int arr[n];
+int i;
+for(i=0;i<n;i++)
+{
+	scanf("%d",&arr[i]);
+}
+radix_sort(arr,n);
+}
+
+
