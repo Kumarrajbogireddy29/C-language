a12819bbb217cc9a09572109f2ea81e7fbb36851
Kumar Raj Bogireddy <kumarrajbogireddy29@gmail.com>
2025-03-22 23:22:24 +0530

    Add files via upload

---
+++ size of operator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	float f;
+	char c;
+	double d;
+	short s;
+	long long ll;
+	printf("%d bytes\n",sizeof(n));
+	printf("%d bytes\n",sizeof(n));
+	printf("%c bytes\n",sizeof(n));
+	printf("%d bytes\n",sizeof(n));
+	printf("%s bytes\n",sizeof(n));
+	printf("%d bytes\n",sizeof(n));
+}
