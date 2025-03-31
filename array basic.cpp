0ec9956162d4a3829559fd89923fb2de89aef531
Kumar Raj Bogireddy <kumarrajbogireddy29@gmail.com>
2025-03-31 23:24:26 +0530

    Add files via upload

---
+++ array basic.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int n=5;
+	int *arr = ((int*(malloc(sizeof(int));
+	for (int i=0;i<5;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(int i=0;i<5;i++){
+		printf("%d",arr[i]);
+	}
+}
+
