5d79a722c96ce4a8bb9b6698aaad9a8500905a29
Kumar Raj Bogireddy <kumarrajbogireddy29@gmail.com>
2025-04-01 23:44:05 +0530

    Add files via upload

---
+++ found in the bovx.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,f=0; 
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	int se;
+	scanf("%d",&se);
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==se)
+		{
+		  f=1;
+		  break;
+		}
+	}
+	if(f==1)
+		{
+			printf("element found\n");
+		}
+	else
+	{
+		printf("element not found\n");
+	}
+} 
+
+	
+
