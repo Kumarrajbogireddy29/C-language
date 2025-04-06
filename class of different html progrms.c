c25232114f2ec47e31587ae2e3ce94508da061e9
Kumar Raj Bogireddy <kumarrajbogireddy29@gmail.com>
2025-04-06 23:16:36 +0530

    Add files via upload

---
+++ class of different html progrms.c
@@ -0,0 +1,258 @@
+<html>
+<head>
+<title>Aditya Surampalem</title>
+</head>
+<body>
+	<h1Aditya Engineering college></h1>
+<img src="../HTML programs/images.aditya.jpg"
+height="200" width="1300">
+</body>
+</html>
+
+<DocType html>
+	<html>
+	<head>
+		<title>special one </title>
+	<body bgcolor="white">
+		<a href="https://www.ferrari.com/en-IN/"target="_blank">click here</a>
+		<center><h1>CARS</center></h1>
+		<img src="../cars/download.jpg"
+		height="300px" width="300px">
+		<p1>FORBES</p1>
+		<img src="../cars/images.jpg"
+		height="300px" width="300px">
+		 <p2>SPORTS CAR</p2>
+		<img src="../cars/images1.jpg"
+		height="300px" width="300px">
+		 <p3> SPORTS CAR</p3>
+		
+	</body>
+</head>
+</html>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<img src="../cars/images.jpg" width="550px" height="550px">
+<a href="image.html">
+clickhere </a>
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<style>
+  table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+</style>
+<body>
+  <table style="width: 70%;">
+    
+    <tbody>
+      <tr>
+        <td colspan="2">     column         </td>
+        
+        <td colspan="2">      column        </td>
+         
+
+              </tr>
+              <tr>
+        <td colspan="2">      column        </td>
+        
+        <td colspan="2">      column        </td>
+        
+
+              </tr>
+              <tr>
+        <td> col </td>
+        <td colspan="2">      column         </td>
+        
+        <td>  col  </td>
+
+              </tr>
+              <tr>
+        <td>  col  </td>
+        <td colspan="2">     column        </td>
+        
+        <td>  col  </td>
+
+              </tr>
+        <tr>
+        <td rowspan="2">  row  </td>
+        <td rowspan="2">  row  </td>
+        <td colspan="2">     column         </td>
+        
+        </tr>
+
+        <tr>
+                
+        <td colspan="2">     column         </td>
+        
+        </tr>
+
+
+
+    </tbody>
+  </table>
+
+</body>
+</html>
+
+
+<html>
+<head>
+<title>Formatting tags</title>
+</head>
+<body>
+	<h1>Technology</h1>
+	<p>
+		<b>Technology</b> is the application of <i>knowledge</i> for 1<sup>st</sup> achieving <strong>practical</strong> goals in a <em>reproducible way</em>.the water(<sub>H20</sup>) The word technology can also mean the products resulting from such <del>efforts, including both</del> tangible tools such as utensils or machines, and <u>intangible ones such as software</u>. <small>Technology</small> plays a critical role in <big>science, engineering</big>, and <mark>everyday life</mark>.
+</body>
+</html>
+
+
+<html>
+<head>
+<title>LISTS </title>
+</head>
+<body>
+	<!--order list-->
+	<ol>
+		<li>Coffee</li>
+		<li>Horlicks</li>
+		<li>Boost</li>
+	</ol>
+	<!--unorder list-->
+	<ul>
+		<li>ADITYA</li>
+		<li>PRAGATI</li>
+		<li>GIET</li>
+	</ul>
+	<dl>
+		<dt>mobiles</dt>
+		<dd>APPLE</dd>
+		<dd>vivo</dd>
+	</dl>
+
+</body>
+</html>
+
+
+<html>
+<head>
+<title>Iframe Tag </title>
+</head>
+<body>
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/IZF-rOe9u-g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d61052.391943593!2d81.77338561009523!3d16.985639037406525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1677836327318!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+	<iframe src="https://www.acoe.edu.in"></iframe>
+</body>
+</html>
+
+
+<html>
+<head>
+<title>style attribute</title>
+<link rel="icon" href="../images.aditya.jpg">
+</head>
+<body bgcolor="black">
+	<h1 style= "color:black;font-size:40px;border:1px solid blue;background-color:red">This is a Aditya</h1>
+</body>
+</html>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Divisions</title>
+</head>
+<body>
+	<center>
+		<div class="parent" style=" background-color:red;width:600px;height:400px;"</dv>
+		      
+		       <div class="a" style="height:200px;width:600px;background-color: green">
+		       </div>
+		       <div class="b" style="height:200px;width:600px;background-color:yellow">
+		       </div>
+</center>
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Divisions</title>
+</head>
+<body>
+	<center>
+		<div class="parent" style=" background-color:red;width:600px;height:400px;">
+		     <div class="a" style="height:400px;width:300px;background-color: green;float: left;"></div>
+		       <div class="b" style="height:400px;width:300px;background-color:yellow;float: right;"></div>
+		</div>
+</center>
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html>
+<head>DIV CLASS
+	<title></title>
+</head>
+<body>
+	<center>
+	<div class="parent" style="background-color:white;height:1000px;width:600px;">
+		<div class="a" style="background-color:red;height:400px;width:px;"></div>
+		<div class="b" style="background-color:blue;height:400px;width:500px;"></div>
+  		<div class="c" style="background-color:red;height:200px;width:250px;">
+  		</div>
+  	</div>
+    </center>
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Div Task</title>
+</head>
+<body>
+	<center>
+		<div class="main" style="background-color:white;height:500px;width:800px ;">
+			<div class="a" style="background-color:gray;height:20px;width: 390px;float: left"></div>
+			<div class="a" style="background-color:ghostwhite;height:20px;width: 10px;float: left;"></div>
+			<div class="a" style="background-color:red;height:20px;width: 90px;float: left"></div>
+
+			<div class="a" style="background-color:ghostwhite;height:20px;width: 10px;float: left;"></div>
+			<div class="a" style="background-color:green;height:20px;width: 300px;float: left"></div>
+			<div class="a" style="background-color:ghostwhite;height:20px;width: 800px;float: left;"></div>
+			<div class="b" style="background-color:pink;height:100px;width: 400px;float: left;"></div>
+			<div class="a" style="background-color:ghostwhite;height:100px;width: 20px;float: left;"></div>
+			<div class="b" style="background-color:pink;height:140px;width: 800px;"></div>
+			<div class="a" style="background-color:ghostwhite;height:20px;width: 800px;float: left;"></div>
+			<div class="c" style="background-color:blue;height: 80px;width:150px ; float: left;"></div>
+			<div class="c" style="background-color:ghostwhite; height: 80px; width: 10px;float: left;"></div>
+			<div class="c" style="background-color:blue;height: 80px;width:150px ; float: left;"></div>
+			<div class="c" style="background-color:ghostwhite; height: 80px; width: 10px;float: left;"></div>
+			<div class="c" style="background-color:blue;height: 80px;width:270px ; float: left;"></div>
+			<div class="c" style="background-color:ghostwhite; height: 80px; width: 10px;float: left;"></div>
+			<div class="c" style="background-color:blue;height: 80px;width:200px ; float: left;"></div>
+			<div class="a" style="background-color:ghostwhite;height:20px;width: 800px;float: left;"></div>
+			<div class="d" style="background-color:green; height: 40px;width: 590px;float: left;"></div>
+			<div class="d" style="background-color:ghostwhite;height: 40px;width: 10px;float: left;"></div>
+			<div class="b" style="background-color:purple;height: 40px;width: 200px;float: left;"></div>
+		</div>
+	</center>
+</body>
+</html>
